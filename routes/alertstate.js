5e2ba159477752bfae373381f3e3f28a8cf8800c
HoyeonKim <cabinkhy@gist.ac.kr>
2022-10-07 02:30:26 +0100

    backend send detection result

--- routes/alertstate.js
+++ routes/alertstate.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs')
 
 /* GET home page. */
 router.get('/backend', function(req, res, next) {
   console.log('Hello')
+  var detect_result = fs.readFileSync('/home/softbeep/Desktop/image_detect/detect_log.json')
+  detect_result = JSON.parse(detect_result)
+  res.json(detect_result)
   //res.render('index', { title: 'Exptrdd' });
-  res.json([{ id: 1, username: "daisy" }]);
+  //res.json([{ id: 1, username: "daisy" }]);
 });
 
 module.exports = router;
